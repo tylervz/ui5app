541a3286a21cc7650dc346dce80f20520988c152
Tyler VanZanten <vanzanten.tyler@gmail.com>
2020-06-02 10:34:25 -0500

    Remove the cordova-plugin-speechrecognition plugin and any uses of it in the app to see if it might have been causing an issue with WkWebView, but the UI5 app is still not loading

--- www/controller/BaseController.js
+++ www/controller/BaseController.js
@@ -1,56 +1,57 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller", 
+    "sap/ui/core/mvc/Controller",
     "com/noki_online/ui5app/util/SpeechRecognition",
-    "com/noki_online/ui5app/util/formatter"    
+    "com/noki_online/ui5app/util/formatter"
 ], function (Controller, SpeechRecognition, formatter) {
     "use strict";
 
         return Controller.extend("com.noki_online.ui5app.controller.BaseController", {
 
-            formatter: formatter, 
+            formatter: formatter,
 
             getRouter : function () {
-				return sap.ui.core.UIComponent.getRouterFor(this);
-			},
+        return sap.ui.core.UIComponent.getRouterFor(this);
+      },
 
             getModel : function (sName) {
-				return this.getView().getModel(sName);
-			},
+        return this.getView().getModel(sName);
+      },
 
             setModel : function (oModel, sName) {
-				return this.getView().setModel(oModel, sName);
-			},
+        return this.getView().setModel(oModel, sName);
+      },
 
             getResourceBundle : function () {
-				return this.getOwnerComponent().getModel("i18n").getResourceBundle();
-            }, 
+        return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+            },
 
             _prepareSpeechRecognition: function() {
                 var oConfig = this.getOwnerComponent().getModel("config");
-                SpeechRecognition.checkAvailability().then(function() {
-    
-                    // SpeechRecognition is available
-                    oConfig.setProperty("/SpeechRecognitionAvailable", true);
-    
-                    SpeechRecognition.checkPermission().then(function() {
-                        // SpeechRecognition permission is set
-                        oConfig.setProperty("/SpeechRecognitionAllowed", true);
-                    }).catch(function(sError) {
-                        // We have no permission on SpeechRecognition
-                        if (sError === "SystemDenied") {
-                            oConfig.setProperty("/SystemDeniedAccess", true);
-                        }
-                        oConfig.setProperty("/SpeechRecognitionAllowed", false);
-                    })
-                }).catch(function(error) {
-                    // no SpeechRecognition available
-                    // alert("Help, no microphone available :-(");
-                    console.log(error);
-                    oConfig.setProperty("/SpeechRecognitionAvailable", false);
-                });
+                oConfig.setProperty("/SpeechRecognitionAllowed", false);
+                // SpeechRecognition.checkAvailability().then(function() {
+
+                //     // SpeechRecognition is available
+                //     oConfig.setProperty("/SpeechRecognitionAvailable", true);
+
+                //     SpeechRecognition.checkPermission().then(function() {
+                //         // SpeechRecognition permission is set
+                //         oConfig.setProperty("/SpeechRecognitionAllowed", true);
+                //     }).catch(function(sError) {
+                //         // We have no permission on SpeechRecognition
+                //         if (sError === "SystemDenied") {
+                //             oConfig.setProperty("/SystemDeniedAccess", true);
+                //         }
+                //         oConfig.setProperty("/SpeechRecognitionAllowed", false);
+                //     })
+                // }).catch(function(error) {
+                //     // no SpeechRecognition available
+                //     // alert("Help, no microphone available :-(");
+                //     console.log(error);
+                //     oConfig.setProperty("/SpeechRecognitionAvailable", false);
+                // });
             }
 
 
-        }); 
+        });
     }
 );
--- www/controller/Record.controller.js
+++ www/controller/Record.controller.js
@@ -1,16 +1,16 @@
 sap.ui.define([
 		"com/noki_online/ui5app/controller/BaseController",
-		"com/noki_online/ui5app/util/SpeechRecognition", 
+		"com/noki_online/ui5app/util/SpeechRecognition",
 		"sap/ui/model/json/JSONModel"
 	], function(BaseController, SpeechRecognition, JSONModel){
-		
+
 	"use strict";
-	
+
 	return BaseController.extend("com.noki_online.ui5app.controller.Record", {
-		
+
 		onInit: function() {
 			this.getRouter().getRoute("record").attachPatternMatched(this._onRouteMatched, this);
-		}, 
+		},
 
 		_onRouteMatched: function() {
 			this._createViewModel([]);
@@ -18,7 +18,7 @@ sap.ui.define([
 
 		onRecordText: function() {
 			var bMobile = this.getModel("config").getProperty("/SpeechRecognitionAvailable");
-			var bSystemDeniedAccess = this.getModel("config").getProperty("/SystemDeniedAccess"); 
+			var bSystemDeniedAccess = this.getModel("config").getProperty("/SystemDeniedAccess");
 			this._runRecordTimer();
 
 			if (!bMobile || bSystemDeniedAccess) {
@@ -34,20 +34,20 @@ sap.ui.define([
 		},
 
 		onStopVoice: function() {
-			SpeechRecognition.stopRecording();
+			// SpeechRecognition.stopRecording();
 			this.getModel("viewModel").setProperty("/recordingRunning", false);
 		},
 
 		_recordVoice: function() {
 			this.getModel("viewModel").setProperty("/recordingRunning", true);
-			SpeechRecognition.startRecording().then(function(aResult) {
-				if (aResult && aResult.length > 0) {
-					this.getModel("viewModel").setProperty("/textInput", aResult[0]);
-					this._createEntry();	
-				}
-				console.log(oResult);
-			}.bind(this));
-		}, 
+			// SpeechRecognition.startRecording().then(function(aResult) {
+			// 	if (aResult && aResult.length > 0) {
+			// 		this.getModel("viewModel").setProperty("/textInput", aResult[0]);
+			// 		this._createEntry();
+			// 	}
+			// 	console.log(oResult);
+			// }.bind(this));
+		},
 
 		_openManualInput: function() {
 
@@ -56,14 +56,14 @@ sap.ui.define([
 
 			if (!this._oManualInputDialog) {
 				this._oManualInputDialog = sap.ui.xmlfragment("com.noki_online.ui5app.view.fragments.EnterTextDialog", this);
-				this.getView().addDependent(this._oManualInputDialog); 
+				this.getView().addDependent(this._oManualInputDialog);
 			}
-			this._oManualInputDialog.open();			
-		}, 
+			this._oManualInputDialog.open();
+		},
 
 		onCloseManualInput: function() {
-			this._oManualInputDialog.close();			
-		}, 
+			this._oManualInputDialog.close();
+		},
 
 		onConfirmInput: function(oEvent) {
 			this._oManualInputDialog.close();
@@ -71,30 +71,30 @@ sap.ui.define([
 
 			this._createEntry();
 
-		}, 
+		},
 
 		_runRecordTimer: function() {
 			var oViewModel = this.getModel("viewModel");
-			
+
 			this.timer = setInterval(function() {
 				var currentTime = parseInt(oViewModel.getProperty("/liveTimer"), 10);
 				oViewModel.setProperty("/liveTimer", currentTime + 1000);
 			}.bind(this), 1000);
-		}, 
+		},
 
 		_stopRecordTimer: function() {
 			clearInterval(this.timer);
-		}, 
+		},
 
 		_getDateTime: function() {
 			var oDate = new Date();
 
 			return this.formatter.formatDate(oDate);
-		}, 
+		},
 
 		_createEntry: function() {
 			this._stopRecordTimer();
-			
+
 			var oViewModel = this.getModel("viewModel");
 			var aTextList = oViewModel.getProperty("/textData");
 			var oListCount = parseInt(aTextList.length, 10) + 1;
@@ -102,35 +102,35 @@ sap.ui.define([
 			var sTime = this.formatter.msToMinutesAndSeconds(sMilliSeconds);
 
 			var oData = {
-				"id": oListCount, 
-				"title": this._getDateTime(), 
-				"time": sTime, 
+				"id": oListCount,
+				"title": this._getDateTime(),
+				"time": sTime,
 				"text": oViewModel.getProperty("/textInput")
 			};
 
 			aTextList.push(oData);
 			this._createViewModel(aTextList);
-		}, 
+		},
 
-		_createViewModel: function(aData) { 
-			
+		_createViewModel: function(aData) {
+
 			var oData = {
 				textInput: "",
 				confirmEnabled: true,
-				textData: aData, 
+				textData: aData,
 				androidDevice: sap.ui.Device.os.android ? true : false,
 				recordingRunning: false,
 				liveTimer: 0
 			};
 
 			this._createModel(oData, "viewModel");
-		}, 
+		},
 
 		_createModel: function(aData, sName) {
 			var oModel = new JSONModel(
-				aData	
+				aData
 			);
-			
+
 			this.setModel(oModel, sName);
 		}
 
